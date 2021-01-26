a144c56a94618b24610a389ec2af8f850de6658c
Dany2298 <dany22986@gmail.com>
2021-01-26 13:53:18 +0000

    Instagram on Footer

--- src/Footer.js
+++ src/Footer.js
@@ -52,7 +52,7 @@ function Footer() {
           <p className="col-sm">
 
             &copy;2020 Neo-Terra | All rights reserved |
-             Terms of use | Cookie policy | About us 
+             Terms of use | Cookie policy | About us | Instagram
             
           </p>
         </div>
