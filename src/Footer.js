a3938a8b14f125b6b1fd4fe3050c0655231c96b3
Hamza <1806401@brunel.ac.uk>
2021-01-29 14:29:56 +0000

    footer changes. still need to finish

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import "./Footer.css"
 import { Link } from 'react-router-dom';
 function Footer() {
   return (
@@ -10,41 +11,33 @@ function Footer() {
         <div className="row">
           <div className="col">
 
-            <h4>NEO-TERRA </h4>
-
-            <h1 className="list">
+            <h2 className="list">
               <li>London, England</li>
               <li>Uxbridge</li>
 
-            </h1>
+            </h2>
           </div>
         
           <div className="col">
-
+            
             <h4><Link to='/' className="Links-footer" >  Home </Link></h4>
+            <h4><Link to ='/' className="Links-footer" >News</Link></h4>
+          </div>
+          
+          <div className="col">
+            <h4><Link to='/Learn' className="Links-footer">  Learn </Link></h4>
+            <h4> <Link to='/Quiz' className="Links-footer" >  Quiz </Link></h4>
+          </div>
 
-            <div className="list">
-              <li>
-              <Link to='/Quiz' className="Links-footer" >  Quiz </Link>
-              </li>
-              <li>
-                 <Link to='/Maps'className="Links-footer" >  Maps </Link>
-               </li>
-              <li>
-              <Link to='/Learn' className="Links-footer">  Learn about climate change </Link>
-              </li>
-              <li>
-              <Link to='/TakeAction'className="Links-footer" >  Take action </Link>
-              </li>
-            </div>
+          <div className="col">
+            <h4><Link to='/TakeAction'className="Links-footer" >  Take action </Link></h4>
+            <h4><Link to='/Maps'className="Links-footer" >  Maps </Link></h4>
           </div>
         
           <div className="col">
             <h4>Contact us</h4>
-            <div className="list">
-              <li>FAQs</li>
-              <li>Contact us</li>
-            </div>
+            <h4>About us</h4>
+
           </div>
         </div>
         <hr />
@@ -52,7 +45,7 @@ function Footer() {
           <p className="col-sm">
 
             &copy;2020 Neo-Terra | All rights reserved |
-             Terms of use | Cookie policy | About us | Instagram
+             Terms of use | Cookie policy | FAQs | Instagram | Contact us
             
           </p>
         </div>
