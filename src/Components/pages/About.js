f9d96975e103d7ca0758c7a1e18554ef176a1fef
Dany2298 <dany22986@gmail.com>
2021-01-31 14:31:57 +0000

    Desc(About Us)

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -15,7 +15,7 @@ function About () {
               <div style={backgroundImage}   >
 
               <h1> WHO WE ARE </h1>
-              
+              <p>Neo-Terra is a group of students who work together to fight against climate changes all over the United Kingdom. We share the news relating the climate changing, we offer you the possibility to check your carbon footprint thanks to our calculator and finally, we have a quiz that you can find on the website to test your knowledge on the subject. We aim to create a new generation of young people who are aware of the situation, educated about society and the change we need, in order to change the system we live in.​ </p>
               </div>
                 
         </Layout>
