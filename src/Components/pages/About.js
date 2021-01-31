936b747bef1effc9d6161a6ed288ba572aa1e448
faresdahmani <faresdahmani10@yahoo.fr>
2021-01-31 14:40:37 +0000

    What we do (rest of the paragraph)

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -22,6 +22,16 @@ function About () {
 Our main goal is to get people to be more involved in climate change and the consequences it has in today’s society. Although it seems to be an issue that is taken into account, we still struggle to mitigate its effects.
 
 The development of our platform will be focused on a younger audience, ranging from 15 to 25 years of age. Almost half of the global population is aged under 25 and although our target audience represents only the 16%, we believe that it is essential to educate and provide resources to a group of people who are developing their critical thinking but yet they play an important role in society.</p>
+In order to accomplish our project, we defined our goal into three main principles, or what we call the “EEE”:
+
+Engage: bring new users to our platform
+Educate: provide information from reliable resources
+Encourage: help them take a course of action providing existing and innovating solutions.
+
+In addition to this, we believe that users should be more exposed to the news, as it is known from research that news has a correlation with the level of concern people have towards environmental issues.
+2019 was the second warmest year on record and the end of the warmest decade (2010- 2019) ever recorded.
+Carbon dioxide (CO2) levels and other greenhouse gases in the atmosphere rose to new records in 2019. 
+Climate change is affecting every country on every continent. It is disrupting national economies and affecting lives. Weather patterns are changing, sea levels are rising, and weather events are becoming more extreme.              
               </div>
                 
         </Layout>
