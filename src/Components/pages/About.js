866a59a817cc2eb7b2945341091cdc90e06b144f
Hamza <1806401@brunel.ac.uk>
2021-01-31 14:03:51 +0000

    about us background

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Layout from '../../Layout/Layout'
+
+const backgroundImage = {
+    backgroundImage : "url(/images/trees1.jpg)",
+    height: "1000px",
+    position: "fit",
+    display: "flex",
+  };
 function About () {
     return(
         <>
             <Layout>
-                <div>
-                About us 
-                </div>
+                <div style={backgroundImage}  ></div>
         </Layout>
         </>
     )
