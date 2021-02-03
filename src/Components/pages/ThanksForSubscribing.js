efe4b07e2e200d4d57718867a19efcfb9549f4aa
Hamza <1806401@brunel.ac.uk>
2021-02-03 18:01:34 +0000

    update

--- src/Components/pages/ThanksForSubscribing.js
+++ src/Components/pages/ThanksForSubscribing.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '../../Layout/Layout'
 import '../../Styles/ThanksForSubscribing.css';
+import { Link } from 'react-router-dom';
+import {Button} from 'antd'
 
 const backgroundImage = {
     backgroundImage : "url(/images/ForestPic.jpg)",
@@ -18,7 +20,12 @@ function ThanksForSubscribing () {
               <div class="centerText">
                 <h1> THANKS FOR SUBSCRIBING </h1>
                 <h1>TO OUR NEWSLETTER</h1>
+                <h1>
+                <button className="continueButton"> <Link to="/Learn"> Continue To Website</Link>
+                </button>
+            </h1>
               </div>
+              
               </div>  
          </Layout>
 
