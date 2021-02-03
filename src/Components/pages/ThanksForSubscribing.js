2847dabf83837276e90a8b568e1a68939e6ef40f
Hamza <1806401@brunel.ac.uk>
2021-02-03 10:55:43 +0000

    Thanks for subscribing

--- src/Components/pages/ThanksForSubscribing.js
+++ src/Components/pages/ThanksForSubscribing.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Layout from '../../Layout/Layout'
+import '../../Styles/ThanksForSubscribing.css';
+
+const backgroundImage = {
+    backgroundImage : "url(/images/ForestPic.jpg)",
+    height: "1000px",
+    display: "flex",
+    width :"1900px"
+};
 
 function ThanksForSubscribing () {
     return(
         <>
          <Layout>
-         <div>
-        <div>
-        Thanks For Subscribing
-        </div>
-        </div>
+        
+         <div style={backgroundImage}>
+              <div class="centerText">
+                <h1> THANKS FOR SUBSCRIBING </h1>
+                <h1>TO OUR NEWSLETTER</h1>
+              </div>
+              </div>  
          </Layout>
 
 
