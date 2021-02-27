470ba033f6c7da4c28944acf205f90b76e543ebc
Deividas <ag.deivis@gmail.com>
2021-02-27 14:47:24 +0000

    fixed bbc news auto generation and fixed nyt auto date generation

--- java_database_code/neo-terra/src/main/java/com/cs2001groupprojectblue27/neoterra/processing/AutomatedNews.java
+++ java_database_code/neo-terra/src/main/java/com/cs2001groupprojectblue27/neoterra/processing/AutomatedNews.java
@@ -34,16 +34,14 @@ public class AutomatedNews {
     }
 	public NewsStories bbc() throws IOException
 	{
-		String titleName="blank";
-		String urlName="blank"; 
-		String dateName="blank"; 
+		String titleName="Unavailable";
+		String urlName="Unavailable"; 
+		String dateName="Unavailable"; 
 		boolean alreadyExists=false;
 		
 		long nr=1;
 		
 		long pageNr=1;
-		
-		
 		
 		while(alreadyExists==false)
 		{
@@ -54,22 +52,21 @@ public class AutomatedNews {
 				nr=1;	
 			}
 			Document doc = Jsoup.connect("https://www.bbc.co.uk/search?q=climate+change&page="+pageNr).get();
-			
-			Elements title = doc.select("#main-content > div > div.ssrcss-1v7bxtk-StyledContainer.eu4yxp60 > div > div > ul > li:nth-child("+nr+") > div > div > div.ssrcss-v4rel9-PromoContent.e1f5wbog0 > div.ssrcss-l100ew-PromoContentSummary.e1f5wbog1 > p.ssrcss-1uw1j0b-PromoHeadline.e1f5wbog2 > a");
+			Elements title = doc.select("#main-content > div > div.ssrcss-1v7bxtk-StyledContainer.eu4yxp60 > div > div > ul > li:nth-child("+nr+") > div > div > div.ssrcss-1bn8j6y-PromoContent.e1f5wbog0 > div.ssrcss-1mcauj9-Stack.e1y4nx260 > a");
 			titleName=title.text();
-			if(repository.existsByStoryTitle(titleName)==false&&titleName!=null&&titleName!=""&&titleName.isBlank()==false&&titleName.isEmpty()==false)
+			if(repository.existsByStoryTitle(titleName)==false&&titleName!=null&&titleName!=""&&titleName.isEmpty()==false)
 			{
-
-				Elements date = doc.select("#site-content > div > div:nth-child(2) > div.css-46b038 > ol > li:nth-child("+nr+") > div > span");
+				
+											
+				Elements date = doc.select("#main-content > div > div.ssrcss-1v7bxtk-StyledContainer.eu4yxp60 > div > div > ul > li:nth-child("+nr+") > div > div > div.ssrcss-1bn8j6y-PromoContent.e1f5wbog0 > div.ssrcss-1vchkvf-Stack.e1y4nx260 > div > dl > div:nth-child(1) > dd > span > span:nth-child(2)");
 				
 				Element link = doc
-						.select("#main-content > div > div.ssrcss-1v7bxtk-StyledContainer.eu4yxp60 > div > div > ul > li:nth-child("+nr+") > div > div > div.ssrcss-v4rel9-PromoContent.e1f5wbog0 > div.ssrcss-l100ew-PromoContentSummary.e1f5wbog1 > p.ssrcss-1uw1j0b-PromoHeadline.e1f5wbog2 > a")
+						.select("#main-content > div > div.ssrcss-1v7bxtk-StyledContainer.eu4yxp60 > div > div > ul > li:nth-child("+nr+") > div > div > div.ssrcss-1bn8j6y-PromoContent.e1f5wbog0 > div.ssrcss-1mcauj9-Stack.e1y4nx260 > a")
 						.first();
 				String url = link.attr("abs:href");
 				
 				//Elements description = doc.select("#main-content > div > div.ssrcss-1v7bxtk-StyledContainer.eu4yxp60 > div > div > ul > li:nth-child("+nr+") > div > div > div.ssrcss-v4rel9-PromoContent.e1f5wbog0 > div.ssrcss-l100ew-PromoContentSummary.e1f5wbog1 > p:nth-child(2)");
-				
-				dateName=date.toString();
+				dateName=date.text();
 				urlName=url;
 				alreadyExists=true;
 			}
@@ -79,9 +76,9 @@ public class AutomatedNews {
 	}
 	public NewsStories newYorkTimes() throws IOException
 	{
-		String titleName="blank";
-		String urlName="blank";
-		String dateName="blank"; 
+		String titleName="Unavailable";
+		String urlName="Unavailable";
+		String dateName="Unavailable"; 
 		boolean alreadyExists=false;
 		
 		long nr=1;
@@ -94,17 +91,15 @@ public class AutomatedNews {
 			titleName=title.text();
 			if(repository.existsByStoryTitle(titleName)==false&&titleName!=null&&titleName!=""&&titleName.isBlank()==false&&titleName.isEmpty()==false)
 			{
-
-				Elements date = doc.select("#site-content > div > div:nth-child(2) > div.css-46b038 > ol > li:nth-child("+nr+") > div > span");
+				Elements date = doc.select("#site-content > div > div:nth-child(2) > div.css-46b038 > ol > li:nth-child("+nr+") > div > div > div > a > span > span.css-bc0f0m");
 				
 				Element link = doc
 						.select("#site-content > div > div:nth-child(2) > div.css-46b038 > ol > li:nth-child("+nr+") > div > div > div > a")
 						.first();
 				String url = link.attr("abs:href");
-				
+				System.out.println(date.text());
 				//Elements description = doc.select("#site-content > div > div:nth-child(2) > div.css-46b038 > ol > li:nth-child("+nr+") > div > div > div > a > p.css-16nhkrn");
-				
-				dateName=date.toString();
+				if(date.text().isEmpty()==false) dateName=date.text();
 				urlName=url;
 				alreadyExists=true;
 			}
