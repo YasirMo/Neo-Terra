67a01ea260935034d7962742926c48a3efd97a9c
faresdahmani <faresdahmani10@yahoo.fr>
2021-03-04 16:39:49 +0000

    Changes

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -19,7 +19,7 @@ function About () {
                 <div className="contentBox">
               <div className="center">
               
-              <p><h1> WHO WE ARE </h1>
+              <p><h1> OUR STORY </h1>
               <p>Neo-Terra is a group of students who work together to fight against climate changes all over the United Kingdom. We share the news relating the climate changing, we offer you the possibility to check your carbon footprint thanks to our calculator and finally, we have a quiz that you can find on the website to test your knowledge on the subject. We aim to create a new generation of young people who are aware of the situation, educated about society and the change we need, in order to change the system we live in.​ </p>
               <h1>OUR MISSION</h1>
               <p>Neo-Terra is our proposal as a team to build a web application that will address climate change, one of the UN sustainable development goals.
