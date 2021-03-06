5840b6ae920a80ee8daa45dea264614cc2fb31c7
Paul <paulfernando200112@gmail.com>
2021-03-06 16:39:17 +0000

    Update Navbar.js
    Added few comments for reference and changed button style for the learn articles

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -14,87 +14,71 @@ function Navbar(pros) {
 
   const handleClick = () => setClick(!click);
 
-
-
 return (
-  
-  
-    
-      <div className='navbar-container'> 
-        <div className="navbar-Logo">
-          <a href ='/'> 
-            <img className="navbar-Logo" src='/images/Logo.png' alt='logo' />
-          </a>
-        </div>
-
-        {/* <div className="MidSection"></div> */}
-        
-          <ul className={click ? "nav-menubar active" : "nav-menubar"}>
-          <li className='nav-item'>
-            <a  href='/Quiz' className='nav-links' onClick={handleClick}>
-                Quiz
-            </a>
-           
-          </li>
-          <li className='nav-item' >
-            <a href='/News' className='nav-links' onClick={handleClick}>
-                News
-            </a>
-           
-          </li>
-          <li className='nav-item'>
-            <a href='/CarbonFootprint' className='nav-links' onClick={handleClick} >
-                CF Calculator
-            </a>
-            
-          </li>
-                
-  
-       
-            <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
-                <DropdownToggle caret  className = 'BlogDropDown' style={{backgroundColor: '#7BE0AD'}} >
-                    Learn More Here!
-                </DropdownToggle>
-                <DropdownMenu style={{backgroundColor: '#C8D3D5',}}>
-                  <DropdownItem header style={{backgroundColor: 'white',}}>Learn about Climate Change Below!
-                  </DropdownItem>
-                  <DropdownItem  className='itemLink'><a href='/Learn' >
-                    Effects in <b>Nature</b>
-                  </a>
-                  </DropdownItem>
-                    
-                  <DropdownItem className='itemLink'>
-                    <a href ='/LearnPageTwo'>
-                    Effects in <b>Society</b>
-                    </a>
-                  </DropdownItem>
-
-                        
-                  <DropdownItem className='itemLink'>
-                    <a href ='/LearnPageThree'>
-                    Ways to <b>Reduce</b> Climate Change
-                    </a>
-                  </DropdownItem>
-                  
-
-                </DropdownMenu>
-              </ButtonDropdown>
-          </ul>
-        
-        
-          <div className="menu-closeicon" onClick={handleClick}>
-            <i className={click ? "fas fa-times":"fas fa-bars"}></i>
-  
-          </div>
+  <div className='navbar-container'> 
+    <div className="navbar-Logo">
+      <a href ='/'> 
+        <img className="navbar-Logo" src='/images/Logo.png' alt='logo' />
+      </a>
     </div>
 
-        
-
-
-  
-
+    {/* <div className="MidSection"></div> */}
     
+    <ul className={click ? "nav-menubar active" : "nav-menubar"}>
+      <li className='nav-item'>
+        <a href='/Quiz' className='nav-links' onClick={handleClick}>
+          Quiz
+        </a>
+      
+      </li>
+      <li className='nav-item' >
+        <a href='/News' className='nav-links' onClick={handleClick}>
+          News
+        </a>
+      
+      </li>
+      <li className='nav-item'>
+        <a href='/CarbonFootprint' className='nav-links' onClick={handleClick} >
+          CF Calculator
+        </a>
+        
+      </li>
 
+      {/* Dropdown Button */}
+      <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
+        {/* Main button */}
+        <DropdownToggle caret  className='BlogDropDown' style={{backgroundColor: '#009e5d', fontWeight: 500, border: 'none'}}>
+          Learn More Here!
+        </DropdownToggle>
+
+        {/* Dropdown Menu (Article Links) */}
+        <DropdownMenu style={{backgroundColor: '#C8D3D5',}}>
+
+          <DropdownItem header style={{backgroundColor: 'white',}}>
+            Learn about Climate Change Below!
+          </DropdownItem>
+
+          {/* Main Links */}
+          <DropdownItem  className='itemLink'>
+            <a href='/Learn'>Effects in <b>Nature</b></a>
+          </DropdownItem>
+            
+          <DropdownItem className='itemLink'>
+            <a href ='/LearnPageTwo'>Effects in <b>Society</b></a>
+          </DropdownItem>
+                
+          <DropdownItem className='itemLink'>
+            <a href ='/LearnPageThree'>Ways to <b>Reduce</b> Climate Change</a>
+          </DropdownItem>
+
+        </DropdownMenu>
+      </ButtonDropdown>
+    </ul>
+  
+    <div className="menu-closeicon" onClick={handleClick}>
+      <i className={click ? "fas fa-times":"fas fa-bars"}></i>
+    </div>
+  </div>
 );
 }
 
